59f4cef63fb861353ac49669dd78b20f0a056091
GosuTek <gosutek@gmail.com>
2025-04-07 16:11:20 +0300

    optimize COO to CSR

--- spmv_three_way.cu
+++ spmv_three_way.cu
@@ -1,7 +1,5 @@
-// clang-format off
 #include <stdio.h>
 #include "lib/mmio.h"
-// clang-format on
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
@@ -24,76 +22,73 @@ struct CSRMatrix {
 
 #define BLOCK_SIZE 3
 
-void convert_coo_to_csr(std::vector<COO_Element> coo, CSRMatrix &matrix) {
-  std::sort(coo.begin(), coo.end(), [](const auto &a, const auto &b) {
-    return std::tie(a.row, a.col) < std::tie(b.row, b.col);
-  });
+void convert_coo_to_csr (std::vector<COO_Element> coo, CSRMatrix& matrix)
+{
+  std::sort (coo.begin(), coo.end(),
+             [] (const auto& a, const auto& b) { return std::tie (a.row, a.col) < std::tie (b.row, b.col); });
 
-  matrix.col_idx.resize(matrix.nnz);
-  matrix.val.resize(matrix.nnz);
-  matrix.row_ptr.resize(matrix.rows + 1, 0);
-  for (const auto &e : coo)
-    matrix.row_ptr[e.row + 1]++;
-  std::partial_sum(matrix.row_ptr.begin(), matrix.row_ptr.end(),
-                   matrix.row_ptr.begin());
+  matrix.col_idx.resize (matrix.nnz);
+  matrix.val.resize (matrix.nnz);
+  matrix.row_ptr.resize (matrix.rows + 1, 0);
 
-  for (const auto &e : coo) {
-    int dest = matrix.row_ptr[e.row]++;
-    matrix.col_idx[dest] = e.col;
-    matrix.val[dest] = e.val;
+  for (size_t i = 0; i < coo.size(); ++i) {
+    const auto& e = coo[i];
+    matrix.row_ptr[e.row + 1]++; // Account for element ONLY on the previous row
+    matrix.col_idx[i] = e.col;
+    matrix.val[i] = e.val;
   }
-  std::rotate(matrix.row_ptr.begin(), matrix.row_ptr.end() - 1,
-              matrix.row_ptr.end());
-  matrix.row_ptr[0] = 0;
+  std::partial_sum (coo.begin(), coo.end(), coo.begin());
 }
 
-CSRMatrix parse_and_convert(const char *filename) {
-  FILE *f;
-  if (!(f = fopen(filename, "r"))) {
-    fclose(f);
-    throw std::runtime_error("Failed to open file");
+CSRMatrix parse_and_convert (const char* filename)
+{
+  FILE* f;
+  if (!(f = fopen (filename, "r"))) {
+    fclose (f);
+    throw std::runtime_error ("Failed to open file");
   }
 
   MM_typecode matcode;
-  if (mm_read_banner(f, &matcode) != 0) {
-    fclose(f);
-    throw std::runtime_error("Couldn't parse matrix");
+  if (mm_read_banner (f, &matcode) != 0) {
+    fclose (f);
+    throw std::runtime_error ("Couldn't parse matrix");
   }
 
-  if (!mm_is_sparse(matcode)) {
-    fclose(f);
-    throw std::runtime_error("CSRMatrix is non-sparse");
+  if (!mm_is_sparse (matcode)) {
+    fclose (f);
+    throw std::runtime_error ("CSRMatrix is non-sparse");
   }
 
   CSRMatrix matrix;
-  if (mm_read_mtx_crd_size(f, &matrix.rows, &matrix.cols, &matrix.nnz) != 0) {
-    fclose(f);
-    throw std::runtime_error("Failed to read matrix size");
+  if (mm_read_mtx_crd_size (f, &matrix.rows, &matrix.cols, &matrix.nnz) != 0) {
+    fclose (f);
+    throw std::runtime_error ("Failed to read matrix size");
   }
 
   std::vector<COO_Element> coo_elements;
-  coo_elements.reserve(matrix.nnz);
+  coo_elements.reserve (matrix.nnz);
   for (int i = 0; i < matrix.nnz; ++i) {
     COO_Element e;
-    fscanf(f, "%u %u %lg\n", &e.row, &e.col, &e.val);
+    fscanf (f, "%u %u %lg\n", &e.row, &e.col, &e.val);
     e.row--;
     e.col--;
-    coo_elements.push_back(e);
+    coo_elements.push_back (e);
   }
-  fclose(f);
+  fclose (f);
 
-  convert_coo_to_csr(std::move(coo_elements),
-                     matrix); // coo_elements no longer exists
+  convert_coo_to_csr (std::move (coo_elements),
+                      matrix); // coo_elements no longer exists
   return matrix;
 }
 
 // Driver
-int main() {
+int main()
+{
   // spmv_global(A, x, rs);
 
   try {
-    CSRMatrix A = parse_and_convert("data/scircuit.mtx");
-  } catch (const std::exception &e) {
+    CSRMatrix A = parse_and_convert ("data/scircuit.mtx");
+  } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
