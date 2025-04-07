88a960a21f653c07362fb457a88d6de0c3c66b40
GosuTek <gosutek@gmail.com>
2025-04-07 18:37:26 +0300

    allocate and copy to dev mem

--- src/spmv_three_way.cu
+++ src/spmv_three_way.cu
@@ -8,19 +8,82 @@
 #include <tuple>
 #include <vector>
 
+#define BLOCK_SIZE 3
+#define VAL_TYPE double
+
 struct COO_Element {
   uint32_t row, col;
-  double val;
+  VAL_TYPE val;
 };
 
 struct CSRMatrix {
   int rows = 0, cols = 0, nnz = 0;
   std::vector<uint32_t> col_idx;
   std::vector<uint32_t> row_ptr;
-  std::vector<double> val;
+  std::vector<VAL_TYPE> val;
 };
 
-#define BLOCK_SIZE 3
+/*
+__global__ void spmv(CSRMatrix A, CSRMatrix x, CSRMatrix rs)
+{
+
+  float rs_value = 0;
+
+  int row = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (row < A.rows) {
+
+    for (int e = 0; e < A.cols; ++e) rs_value += A.elements[row * A.cols + e] * x.elements[e];
+
+    rs.elements[row] = rs_value;
+  }
+}
+*/
+void spmv_global(const CSRMatrix A, const CSRMatrix x, const CSRMatrix rs)
+{
+
+  // Load sparse
+
+  CSRMatrix d_A;
+
+  d_A.cols = A.cols;
+
+  d_A.rows = A.rows;
+
+  uint32_t* col_idx_ptr = d_A.col_idx.data();
+  uint32_t* row_ptr_ptr = d_A.row_ptr.data();
+  VAL_TYPE* val_ptr = d_A.val.data();
+
+  // Allocate for the 3 arrays
+  cudaMalloc(&col_idx_ptr, A.col_idx.size() * sizeof(uint32_t));
+  cudaMalloc(&row_ptr_ptr, A.row_ptr.size() * sizeof(uint32_t));
+  cudaMalloc(&val_ptr, A.val.size() * sizeof(VAL_TYPE));
+
+  // Copy 3 arrays to device
+  cudaMemcpy(col_idx_ptr, A.col_idx.data(), A.col_idx.size() * sizeof(uint32_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(row_ptr_ptr, A.row_ptr.data(), A.row_ptr.size() * sizeof(uint32_t), cudaMemcpyHostToDevice);
+  cudaMemcpy(val_ptr, A.val.data(), A.val.size() * sizeof(VAL_TYPE), cudaMemcpyHostToDevice);
+
+  // Load vector
+  // Allocate result
+  // dim3 dimBlock(BLOCK_SIZE);
+
+  // dim3 dimGrid((A.rows + BLOCK_SIZE - 1) / BLOCK_SIZE);
+
+  // spmv<<<dimGrid, dimBlock>>>(d_A, d_x, d_rs);
+
+  // Copy result to host
+  // cudaMemcpy(rs.elements, d_rs.elements, size, cudaMemcpyDeviceToHost);
+
+  // Deallocate A
+  cudaFree(col_idx_ptr);
+  cudaFree(row_ptr_ptr);
+  cudaFree(val_ptr);
+
+  // Deallocate x
+
+  // Deallocate result
+}
 
 void convert_coo_to_csr(std::vector<COO_Element> coo, CSRMatrix& matrix)
 {
