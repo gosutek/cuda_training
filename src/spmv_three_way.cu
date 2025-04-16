d7a9589bdb5a36ad0c19a22b5ed76ac844b3b39c
GosuTek <gosutek@gmail.com>
2025-04-16 11:51:54 +0300

    refactor spmv_l2_window for new structs

--- src/spmv_three_way.cu
+++ src/spmv_three_way.cu
@@ -19,50 +19,44 @@ __global__ void spmv(const uint32_t* col_idx, const uint32_t* row_ptr, const VAL
   }
 }
 
-// DenseMatrix spmv_l2_window(const CSRMatrix& d_A, const DenseMatrix& d_x, DenseMatrix& d_y)
-// {
-//   cudaStream_t stream;
-//   cudaStreamCreate(&stream);
-//
-//   cudaDeviceProp prop;
-//   cudaGetDeviceProperties(&prop, DEVICE_ID);
-//
-//   size_t size = std::min(int(prop.l2CacheSize * 0.75), prop.persistingL2CacheMaxSize);
-//   cudaDeviceSetLimit(cudaLimitPersistingL2CacheSize,
-//                      size); // cudaLimitPersistingL2CacheSize -> Global limit for all persistent data
-//
-//   size_t window_size
-//     = std::min(prop.accessPolicyMaxWindowSize,
-//                (int)d_x.data_size); // accessPolicyMaxWindowSize -> Per-stream limit for a single persistent region
-//
-//   cudaStreamAttrValue stream_attribute;
-//   stream_attribute.accessPolicyWindow.base_ptr = reinterpret_cast<void*>(d_x.data);
-//   stream_attribute.accessPolicyWindow.num_bytes = window_size;
-//   stream_attribute.accessPolicyWindow.hitRatio = 0.6;
-//   stream_attribute.accessPolicyWindow.hitProp = cudaAccessPropertyPersisting;
-//   stream_attribute.accessPolicyWindow.missProp = cudaAccessPropertyStreaming;
-//
-//   cudaStreamSetAttribute(stream, cudaStreamAttributeAccessPolicyWindow, &stream_attribute);
-//
-//   dim3 dimBlock(BLOCK_SIZE);
-//
-//   dim3 dimGrid((d_A.rows + BLOCK_SIZE - 1) / BLOCK_SIZE);
-//
-//   spmv<<<dimGrid, dimBlock>>>(d_A, d_x, d_y);
-//
-//   // Reset L2 Access to Normal
-//   stream_attribute.accessPolicyWindow.num_bytes = 0;
-//   cudaStreamSetAttribute(stream, cudaStreamAttributeAccessPolicyWindow, &stream_attribute);
-//   cudaCtxResetPersistingL2Cache();
-//
-//   // Allocate host y
-//   DenseMatrix y = DenseMatrix(d_x.rows, d_x.cols);
-//
-//   // Copy y to host
-//   cudaMemcpy(y.data, d_y.data, d_y.data_size, cudaMemcpyDeviceToHost);
-//
-//   return y;
-// }
+DenseMatrix spmv_l2_window(const CSRMatrix& d_A, const DenseMatrix& d_x, DenseMatrix& d_y)
+{
+  cudaStream_t stream;
+  cudaStreamCreate(&stream);
+
+  cudaDeviceProp prop;
+  cudaGetDeviceProperties(&prop, DEVICE_ID);
+
+  size_t size = std::min(int(prop.l2CacheSize * 0.75), prop.persistingL2CacheMaxSize);
+  cudaDeviceSetLimit(cudaLimitPersistingL2CacheSize,
+                     size); // cudaLimitPersistingL2CacheSize -> Global limit for all persistent data
+
+  size_t window_size
+    = std::min(prop.accessPolicyMaxWindowSize,
+               (int)d_x.data_size); // accessPolicyMaxWindowSize -> Per-stream limit for a single persistent region
+
+  cudaStreamAttrValue stream_attribute;
+  stream_attribute.accessPolicyWindow.base_ptr = reinterpret_cast<void*>(d_x.data);
+  stream_attribute.accessPolicyWindow.num_bytes = window_size;
+  stream_attribute.accessPolicyWindow.hitRatio = 0.6;
+  stream_attribute.accessPolicyWindow.hitProp = cudaAccessPropertyPersisting;
+  stream_attribute.accessPolicyWindow.missProp = cudaAccessPropertyStreaming;
+
+  cudaStreamSetAttribute(stream, cudaStreamAttributeAccessPolicyWindow, &stream_attribute);
+
+  dim3 dimBlock(BLOCK_SIZE);
+
+  dim3 dimGrid((d_A.rows + BLOCK_SIZE - 1) / BLOCK_SIZE);
+
+  spmv<<<dimGrid, dimBlock>>>(d_A.col_idx, d_A.row_ptr, d_A.val, d_A.rows, d_x.data, d_y.data);
+
+  // Reset L2 Access to Normal
+  stream_attribute.accessPolicyWindow.num_bytes = 0;
+  cudaStreamSetAttribute(stream, cudaStreamAttributeAccessPolicyWindow, &stream_attribute);
+  cudaCtxResetPersistingL2Cache();
+
+  return DenseMatrix(d_y, AllocTarget::Host);
+}
 
 DenseMatrix spmv_global(const CSRMatrix& d_A, const DenseMatrix& d_x, DenseMatrix& d_y)
 {
@@ -98,10 +92,10 @@ int main()
     DenseMatrix d_y(x, AllocTarget::Device);
     // ================================ KERNEL EXECUTION ================================
     DenseMatrix y_global = spmv_global(d_A, d_x, d_y);
-    // DenseMatrix y_l2_window = spmv_l2_window(d_A, d_x, d_y);
+    DenseMatrix y_l2_window = spmv_l2_window(d_A, d_x, d_y);
 
     std::cout << y_global.data[0] << std::endl;
-    // std::cout << y_l2_window.data[0] << std::endl;
+    std::cout << y_l2_window.data[0] << std::endl;
 
   } catch (const std::exception& e) {
     std::cerr << "Error: " << e.what() << "\n";
